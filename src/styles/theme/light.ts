6c00369941b1236bca3d3cf6f2906823ef58745e
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-05-20 16:27:19 -0300

    feat: Start create themes

---
+++ src/styles/theme/light.ts
@@ -0,0 +1,13 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#D63AF9',
+        secundary: '#C62E65',
+        background: '#F5F5F5',
+        text: '#333'
+
+    },
+
+    // fonts: {}
+}
