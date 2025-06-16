5f615c8527b5f8f2c2587f11b2fe2ce4d557e9d6
Fernando Cardozo <fhpcardozo@gmail.com>
2025-06-16 14:49:14 -0300

    feat: enshrine faunadb

--- src/pages/gameboard.tsx
+++ src/pages/gameboard.tsx
@@ -1,17 +1,17 @@
 import { GetServerSideProps } from "next";
 import { useState } from "react";
 import Head from "next/head";
-import HelpIcon from '@mui/icons-material/Help';
+import HelpIcon from "@mui/icons-material/Help";
 import { Widget } from "../components/Feedback/Widget";
 import { GameBoard } from "../components/GameBoard";
 import { HowToPlayButton } from "../components/HowToPlay/HowToPlayButton";
 import { ScoreBoard } from "../components/ScoreBoard";
-import { ScoreRecords, TableGameSchema } from "../components/ScoreRecords";
+// import { ScoreRecords, TableGameSchema } from "../components/ScoreRecords";
 import { StartGameModal } from "../components/StartGameModal";
 import { api } from "../services/api";
 import { GameBodyContainer } from "../styles/GlobalStyle";
 import { HomeButton } from "../components/HomeButton";
-import { ScoreRecordsModal } from "../components/ScoreRecordsModal";
+// import { ScoreRecordsModal } from "../components/ScoreRecordsModal";
 import { ScoreRecordsModalButton } from "../components/ScoreRecordsModal/ScoreRecordsModalButton";
 import { ToastContainer } from "react-toastify";
 import { ThemeRewardModal } from "../components/ThemeRewardModal";
@@ -19,56 +19,54 @@ import { useTheme } from "../hooks/useTheme";
 import { ThemeProvider } from "styled-components";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-interface GamePageProps {
-    historicalScores: TableGameSchema[]
-}
+// interface GamePageProps {
+//     historicalScores: TableGameSchema[]
+// }
 
-export default function GamePage({ historicalScores }: GamePageProps): JSX.Element {
-
-    const [isStartGameModalOpen, setIsStartGameModalOpen] = useState(true)
-    const [isScoreRecordsModalOpen, setIsScoreRecordsModalOpen] = useState(false)
-    const [isThemeRewardModalOpen, setIsThemeRewardModalOpen] = useState(false)
-    const { theme } = useTheme()
+export default function GamePage(): JSX.Element {
+    const [isStartGameModalOpen, setIsStartGameModalOpen] = useState(true);
+    const [isScoreRecordsModalOpen, setIsScoreRecordsModalOpen] =
+        useState(false);
+    const [isThemeRewardModalOpen, setIsThemeRewardModalOpen] = useState(false);
+    const { theme } = useTheme();
 
     function handleToggleStartGameModal() {
-        setIsStartGameModalOpen(!isStartGameModalOpen)
+        setIsStartGameModalOpen(!isStartGameModalOpen);
     }
 
     function handleOpenScoreRecordsModal() {
-        setIsScoreRecordsModalOpen(!isScoreRecordsModalOpen)
+        setIsScoreRecordsModalOpen(!isScoreRecordsModalOpen);
     }
 
     function handleOpenThemeRewardModal(opening = false) {
-
         if (opening) {
-            setIsThemeRewardModalOpen(true)
+            setIsThemeRewardModalOpen(true);
         } else {
-            setIsThemeRewardModalOpen(!isThemeRewardModalOpen)
+            setIsThemeRewardModalOpen(!isThemeRewardModalOpen);
         }
     }
 
     return (
-        <ThemeProvider
-            theme={theme}
-        >
-
+        <ThemeProvider theme={theme}>
             <GameBodyContainer>
                 <ToastContainer />
                 <Head>
                     <title>GameBoard | ColorsCrush</title>
-                    <meta name="viewport" content=
-                        "width=device-width, user-scalable=no" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, user-scalable=no"
+                    />
                 </Head>
                 <StartGameModal
                     isOpen={isStartGameModalOpen}
                     onRequestClose={handleToggleStartGameModal}
                 />
-
+                {/* 
                 <ScoreRecordsModal
                     isOpen={isScoreRecordsModalOpen}
                     onRequestClose={handleOpenScoreRecordsModal}
                     historicalScores={historicalScores}
-                />
+                /> */}
 
                 <ThemeRewardModal
                     isOpen={isThemeRewardModalOpen}
@@ -77,10 +75,14 @@ export default function GamePage({ historicalScores }: GamePageProps): JSX.Eleme
 
                 <HomeButton />
 
-                <ScoreBoard handleOpenThemeRewardModal={handleOpenThemeRewardModal} />
+                <ScoreBoard
+                    handleOpenThemeRewardModal={handleOpenThemeRewardModal}
+                />
                 <GameBoard />
-                <ScoreRecords historical={historicalScores} />
-                <ScoreRecordsModalButton handleOpenScoreRecordsModal={handleOpenScoreRecordsModal} />
+                {/* <ScoreRecords historical={historicalScores} /> */}
+                <ScoreRecordsModalButton
+                    handleOpenScoreRecordsModal={handleOpenScoreRecordsModal}
+                />
                 <Widget />
 
                 <HowToPlayButton>
@@ -88,27 +90,16 @@ export default function GamePage({ historicalScores }: GamePageProps): JSX.Eleme
                 </HowToPlayButton>
             </GameBodyContainer>
         </ThemeProvider>
-    )
+    );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-
-    const response = await api.get(`${process.env.HOME_PAGE_URL}/api/prisma_api`)
-
-    const historicalScores: TableGameSchema[] = response.data.games.map(game => {
-        return {
-            id: game[3],
-            playerName: game[1],
-            score: game[0],
-            theme: game[2],
-            currentGame: false
-        }
-    })
-
     return {
         props: {
-            historicalScores,
-            ...(await serverSideTranslations(locale, ['gameboard', 'feedback']))
-        }
-    }
-}
+            ...(await serverSideTranslations(locale, [
+                "gameboard",
+                "feedback",
+            ])),
+        },
+    };
+};
